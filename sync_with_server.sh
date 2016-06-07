5156c4b17bd709007f8f1646a496a0e2d1baa2d1
Matthias Koenig <konigmatt@googlemail.com>
2016-06-07 17:56:13 +0200

    better docs

--- sync_with_server.sh
+++ sync_with_server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# plays the changes in koenig to the server
-# SSH access to the server is provided
-########################################
+################################################################################
+# Synchronize changes in homepage with server.
+# SSH access to server is required.
+# ! do not commit any private data/password/codes in the repository !
+################################################################################
 # Server-Name: h2389049.stratoserver.net
 # Login: root
 # Heimatverz.: /root
 # WWW-Verz.: /var/www/html
-########################################
+################################################################################
 
 rsync -rtvuc --delete ./livermetabolism/_site/ mkoenig@h2389049.stratoserver.net:/var/www/html
 
